e00bcca94bf19167b6a3e5539c8de1844f7bbf2e
Bach HG <87762857+radna0@users.noreply.github.com>
2023-05-05 12:17:36 +0700

    New POC

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import * as THREE from 'three';
 import { GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader"
 import { OrbitControls} from "three/examples/jsm/controls/OrbitControls"
 
+const pi = Math.PI
 
 const scene = new THREE.Scene()
 
@@ -15,81 +16,57 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
-camera.position.setZ(30)
+camera.position.x = 15
+camera.position.y = 40
+camera.position.z = -20
 
+renderer.setClearColor( 0xffffff, 0);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-
-const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 })
-
-const torus = new THREE.Mesh(geometry, material)
-
-const book = new URL("./assets/book/scene.gltf", import.meta.url)
-const candlestick = new URL("./assets/candlestick/scene.gltf", import.meta.url)
-const inkBottle = new URL("./assets/ink_bottle/scene.gltf", import.meta.url)
-const woodenTable = new URL("./assets/wooden_table/scene.gltf", import.meta.url)
-
+const laptop = new URL("./assets/laptop/laptop.glb", import.meta.url)
+const room = new URL("./assets/room/office.glb", import.meta.url)
 
 const assetLoader = new GLTFLoader()
-
-assetLoader.load(book.href, (gltf) => {
+var mixer
+assetLoader.load(laptop.href, (gltf) => {
   const model = gltf.scene
-  model.scale.x = 5
-  model.scale.y = 5
-  model.scale.z = 5
-  model.rotation.y = 3.14 / 2
   scene.add(model)
-  model.position.set(10,17.4,-33)
+  model.rotation.y = pi / 2
+  model.position.set(-13,4,1.5)
+  mixer = new THREE.AnimationMixer(model)
+  const clips = gltf.animations
+  const clip = THREE.AnimationClip.findByName(clips, "Scene")
+  const action = mixer.clipAction(clip)
+  action.play()
 })
-assetLoader.load(candlestick.href, (gltf) => {
+assetLoader.load(room.href, (gltf) => {
   const model = gltf.scene
-  model.scale.x = 0.01
-  model.scale.y = 0.01
-  model.scale.z = 0.01
   scene.add(model)
-  model.position.set(3,16.7,-18)
-})
-// assetLoader.load(inkBottle.href, (gltf) => {
-//   const model = gltf.scene
-//   model.scale.x = 70
-//   model.scale.y = 70
-//   model.scale.z = 70
-//   model.rotation.y = 2.5
-//   scene.add(model)
-//   model.position.set(13,18.2,-15)
-// })
-assetLoader.load(woodenTable.href, (gltf) => {
-  const model = gltf.scene
-  model.scale.x = 10
-  model.scale.y = 10
-  model.scale.z = 10 
-  scene.add(model)
-  model.position.set(0,0,0)
+  model.scale.x = 20
+  model.scale.y = 20
+  model.scale.z = 20
+  model.position.set(0,40,0)
 })
 
-scene.add(torus)
 
-const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(3,24,-18)
+const pointLight = new THREE.PointLight(0xffffff,0)
+pointLight.position.set(50,30,0)
 
 const ambientLight = new THREE.AmbientLight(0xffffff)
-scene.add(pointLight, ambientLight)
+scene.add(pointLight,ambientLight)
 
 const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(10000,50)
+const gridHelper = new THREE.GridHelper(200,50)
 scene.add(lightHelper,gridHelper)
-
-
 
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
+const clock = new THREE.Clock()
 function animate() {
   requestAnimationFrame(animate)
+  if (mixer)
+      mixer.update(clock.getDelta())
   
-  torus.rotation.x += 0.01
-  torus.rotation.y += 0.005
-  torus.rotation.z += 0.01
   controls.update()
   renderer.render(scene,camera)
 }
